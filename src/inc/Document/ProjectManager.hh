542852cd8e48ef4519dfe91c0c977cbc80c32640
Rudrabhoj Bhati <rudrabhoj@gmail.com>
2016-08-05 19:37:04 +0530

    Some changes to Document.ProjectManager, full scale assault on ProjectManager comming tomorrow.

--- src/inc/Document/ProjectManager.hh
+++ src/inc/Document/ProjectManager.hh
@@ -1,4 +1,4 @@
-#ifndef __DOCUMENT_PROJECTMANAGER_H__
+5#ifndef __DOCUMENT_PROJECTMANAGER_H__
 #define __DOCUMENT_PROJECTMANAGER_H__
 
 #include <Document/Page.hh>
@@ -7,11 +7,27 @@
 class ProjectManager{
 
 public:
+  ProjectManager(QString projectRootDir);
+
   void setName(QString name);
+  void setSaveState(bool staat);
+  void setprojectFileUrl(QString url);
+
   QString getName();
+  bool getSaveState();
+
+  void addPage(QString imageName);
+  void saveProject();
+  void openProject(QString projectFileLink);
 
 private:
+  QString name;
   QVector<Page> pageList;
+  bool saveState;
+  QString projectRoot;
+  QString projectFileUrl;
+
+  void moveProjectUrl(QString newUrl);
 };
 
 #endif
